01f5358e435b09890c8988e394d3cd74974919bd
Swarnendu11 <rajlayek764@gmail.com>
2026-07-22 12:16:54 +0530

    Updated emp dashboard

--- src/pages/employee/EmployeeDashboard.jsx
+++ src/pages/employee/EmployeeDashboard.jsx
@@ -12,10 +12,44 @@ const EmployeeDashboard = () => {
   try {
     setLoading(true)
 
-    const res = await api.get("/api/dashboard/employee");
-    setData(res.data)
+    const [tasksRes, leavesRes, wfhRes] = await Promise.all([
+      api.get("/api/tasks/mytasks"),
+      api.get("/api/leaves/my"),
+      api.get("/api/wfh/my"),
+    ])
+    const tasks = tasksRes.data
+    const leaves = leavesRes.data
+    const wfh = wfhRes.data
+    
+    setData({
+      totalTasks: tasks.length,
+      completedTasks: tasks.filter(
+        (task) => task.status === "Completed"
+      ).length,
+      pendingTasks: tasks.filter(
+        (task) => task.status ==="Pending"
+      ).length,
+
+      totalLeaves: leaves.length,
+      approvedLeaves: leaves.filter(
+        (leave) => leave.status === "Approved"
+      ).length,
+      rejectedLeaves: leaves.filter(
+        (leave) => leave.status === "Rejected"
+      ).length,
+
+      totalWFH: wfh.length,
+      approvedWFH: wfh.filter(
+        (item) => item.status === "Approved"
+      ).length,
+      rejectedWFH: wfh.filter(
+        (item) => item.status === "Rejected"
+      ).length,
+    })
   } catch (error) {
-    setError("Failed to load dashboard.")
+    console.log(error)
+    console.log(error.response);
+    
     toast.error(
       error?.response?.data?.message ||
       "Failed to load dashboard."
@@ -48,26 +82,68 @@ const EmployeeDashboard = () => {
     <div>
       <h1 className="text-3xl font-bold mb-6">Employee Dashboard</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
+        <div className="bg-blue-100 p-5 rounded-xl shadow">
+          <h2 className="text-lg font-semibold">Total Tasks</h2>
+          <p className="text-3xl font-bold mt-2">
+              {data?.totalTasks ?? 0}
+          </p>
+        </div>
         <div className="bg-green-100 p-5 rounded-xl shadow">
           <h2 className="text-lg font-semibold">Completed Tasks</h2>
-          <p className="text-3xl font-bold mt-2">{data.completedTasks ?? 0}</p>
+          <p className="text-3xl font-bold mt-2">{data?.completedTasks ?? 0}</p>
         </div>
 
         <div className="bg-yellow-100 p-5 rounded-xl shadow">
           <h2 className="text-lg font-semibold">Pending Tasks</h2>
-          <p className="text-3xl font-bold mt-2">{data.pendingTasks ?? 0}</p>
+          <p className="text-3xl font-bold mt-2">{data?.pendingTasks ?? 0}</p>
+        </div>
+
+        <div className="bg-purple-100 p-5 rounded-xl shadow">
+          <h2 className="text-lg font-semibold">Total Leaves</h2>
+          <p className="text-3xl font-bold mt-2"> 
+            {data?.totalLeaves ?? 0}
+          </p>
         </div>
 
         <div className="bg-blue-100 p-5 rounded-xl shadow">
           <h2 className="text-lg font-semibold">Approved Leaves</h2>
-          <p className="text-3xl font-bold mt-2">{data.approvedLeaves ?? 0}</p>
+          <p className="text-3xl font-bold mt-2">{data?.approvedLeaves ?? 0}</p>
         </div>
 
         <div className="bg-orange-100 p-5 rounded-xl shadow">
           <h2 className="text-lg font-semibold">Pending Leaves</h2>
-          <p className="text-3xl font-bold mt-2">{data.pendingLeaves ?? 0}</p>
+          <p className="text-3xl font-bold mt-2">{data?.pendingLeaves ?? 0}</p>
         </div>
+
+        <div className="bg-red-100 p-5 rounded-xl shadow">
+         <h2 className="text-lg font-semibold">Rejected Leaves</h2>
+         <p className="text-3xl font-bold mt-2">
+           {data?.rejectedLeaves ?? 0}
+         </p>
+        </div>
+
+        <div className="bg-indigo-100 p-5 rounded-xl shadow">
+         <h2 className="text-lg font-semibold">Total WFH Requests</h2>
+         <p className="text-3xl font-bold mt-2">
+          {data?.totalWFH ?? 0}
+         </p>
+        </div>
+        
+        <div className="bg-emerald-100 p-5 rounded-xl shadow">
+          <h2 className="text-lg font-semibold">   Approved WFH Requests</h2>
+          <p className="text-3xl font-bold mt-2">
+            {data?.approvedWFH ?? 0}
+          </p>
+        </div>
+        
+      <div className="bg-pink-100 p-5 rounded-xl shadow">
+        <h2 className="text-lg font-semibold">Rejected WFH Requests</h2>
+        <p className="text-3xl font-bold mt-2">
+          {data?.rejectedWFH ?? 0}
+        </p>
+      </div>
+        
       </div>
     </div>
   );
